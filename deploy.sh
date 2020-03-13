1a7596875571f9553a9b44b3d6884a0be0809c09
yoojene <eugene.cross@gmail.com>
2020-03-13 12:11:21 +0000

    reinited git and remote deploy

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 
   git init
 
-  git remote add deploy "travis@genox.io:/var/www/genox.io/html"
+  git remote add deploy "travis@genox.io:/var/www/genox.io"
   git config user.name "Travis CI"
   git config user.email "travisci@genox.io"
 
   git add .
   git commit -m "Deploy"
   git push --force deploy master
-  
+
 else
   echo "Not deploying, since this branch isn't master."
 fi
