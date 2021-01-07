1465ecc92c25951fb45765ac770a1cf66ca9eccc
yoojene <eugene.cross@gmail.com>
2021-01-07 11:17:25 +0000

    deploy script moving all assets

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ FOLDER="../var/www/genox.io/html"
 sshpass -p $PERSONAL_SITE sftp root@$SITEIP << !
 cd $FOLDER
 put -r css/
+put -r font-awesome/
+put -r fonts/
 put -r img/
+put -r js/
 put index.html
 !
 echo 'Deploy complete!'
